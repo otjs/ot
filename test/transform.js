b319c85945ce9c5f3335131cb23f76bc030b916b
luin <i@zihua.li>
2015-05-11 10:06:03 +0800

    Fix a transform bug

--- test/transform.js
+++ test/transform.js
@@ -25,4 +25,77 @@ describe('transform', function() {
       ]
     ]);
   });
+
+  it('should transform correctly 2', function() {
+    var res = transform.transformBatch([{
+      "type": "del",
+      "line": 11
+    }, {
+      "type": "add",
+      "line": 11,
+      "text": "321312321312"
+    }, {
+      "type": "del",
+      "line": 4
+    }, {
+      "type": "del",
+      "line": 3
+    }, {
+      "type": "del",
+      "line": 2
+    }, {
+      "type": "add",
+      "line": 2,
+      "text": "32312"
+    }, {
+      "type": "add",
+      "line": 3,
+      "text": "13321213"
+    }], [{
+      "type": "del",
+      "line": 11
+    }, {
+      "type": "add",
+      "line": 11,
+      "text": "32131232112"
+    }, {
+      "type": "del",
+      "line": 8
+    }, {
+      "type": "add",
+      "line": 8,
+      "text": "1231223"
+    }, {
+      "type": "del",
+      "line": 3
+    }, {
+      "type": "del",
+      "line": 2
+    }, {
+      "type": "add",
+      "line": 2,
+      "text": "3221313"
+    }
+    ]);
+
+    expect(res).to.eql([
+      [
+        null,
+        { type: 'add', line: 11, text: '321312321312' },
+        { type: 'del', line: 3 },
+        null,
+        null,
+        { type: 'add', line: 3, text: '32312' },
+        { type: 'add', line: 4, text: '13321213' }
+      ], [
+        null,
+        { type: 'add', line: 10, text: '32131232112' },
+        { type: 'del', line: 7 },
+        { type: 'add', line: 7, text: '1231223' },
+        null,
+        null,
+        { type: 'add', line: 2, text: '3221313' }
+      ]
+    ]);
+  });
 });
